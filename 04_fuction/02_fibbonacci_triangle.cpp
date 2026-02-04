39952412207ae59e396df5ea156b92ebe349d9cd
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-04 14:28:16 +0530

    fibbo in triangle

---
+++ 04_fuction/02_fibbonacci_triangle.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int fibbo(int n){
+    if(n==0 or n==1){
+        return n;
+    }
+    return fibbo(n-1)+fibbo(n-2);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int g=0;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<fibbo(g)<<"\t";
+            g++;
+        }
+        cout<<endl;
+    }
+}
