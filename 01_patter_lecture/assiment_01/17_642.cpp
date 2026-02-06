403fd0f3f4f2c7ab0a2fc07b614d48b9e52c8128
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-06 13:54:59 +0530

    pascal

--- 01_patter_lecture/assiment_01/17_642.cpp
+++ 01_patter_lecture/assiment_01/17_642.cpp
@@ -3,20 +3,19 @@ using namespace std;
 
 int main(){
     int n;
+
     cin>>n;
     int m=n/2+1;
     for(int i=1;i<=m;i++){
         for(int j=1;j<=n;j++){
-            if(j==1 || j==m || i==m || i==1 && j>m){
+            if(j==1 || j==m || i==m || (i==1 && j>4)){
                 cout<<"*";
             }
             else{
                 cout<<" ";
             }
-            if(i==1 && j>=m){
-                cout<<"*";
-            }
         }
         cout<<endl;
     }
+    
 }
