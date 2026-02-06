5c02253cd9b1f2e2d4faae4d41f6a4d2bb17e8ba
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-06 13:20:13 +0530

    test hackthone

--- 01_patter_lecture/lecture_12/01_square_1236.cpp
+++ 01_patter_lecture/lecture_12/01_square_1236.cpp
@@ -8,16 +8,43 @@ int main(){
 
         //space
         for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+            cout<<"q"<<" ";
         }
         for(int m=1;m<=2*i-1;m++){
             if(i==1){
                 cout<<i<<" ";
             }
+           
             else{
                 
-                    if(m==1 || m==i){
-                        cout<<"h ";
+                    if(m==1 || m==2*i-1){
+                      //  int t=i-1;
+                        cout<<i-1<<" ";
+                    }
+                    else{
+                        cout<<"w ";
+                    }
+                
+            }
+        }
+        cout<<endl;
+    }
+     for(int i=n-1;i>=1;i--){
+
+        //space
+        for(int j=1;j<=n-i;j++){
+            cout<< ";
+        }
+        for(int m=1;m<=2*i-2;m++){
+            if(i==1){
+                cout<<i<<" ";
+            }
+           
+            else{
+                
+                    if(m==1 || m==2*i-1){
+                      //  int t=i-1;
+                        cout<<i<<" ";
                     }
                     else{
                         cout<<"  ";
@@ -28,3 +55,5 @@ int main(){
         cout<<endl;
     }
 }
+
+   
