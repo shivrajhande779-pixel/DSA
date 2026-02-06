09744c647bc4696f1a178ca2122c4c25d71faa0d
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-06 14:14:07 +0530

    pascal

--- 01_patter_lecture/assiment_01/17_642.cpp
+++ 01_patter_lecture/assiment_01/17_642.cpp
@@ -7,15 +7,25 @@ int main(){
     cin>>n;
     int m=n/2+1;
     for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){
-            if(j==1 || j==m || i==m || (i==1 && j>4)){
-                cout<<"*";
-            }
-            else{
+        
+            for(int j=1;j<=n;j++){
+                // if(i<=m){
+                if(j==1 || j==m || i==m || (i==1 && j>m)){
+                    cout<<"*";
+                }
+                else{
                 cout<<" ";
-            }
-        }
+                }
+         //   }
+        
+            
+        // else{
+        //     if(j==m || i==m ){
+        //         cout<<"p";5
+        //     }
+        // }
         cout<<endl;
+    }     
     }
     
 }
