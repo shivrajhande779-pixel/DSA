37cd8a2c00dfc7b2c0321757574d4955ae72e613
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-10 15:58:24 +0530

    vector

--- 06_vector/01_add.cpp
+++ 06_vector/01_add.cpp
@@ -9,8 +9,25 @@ int main(){
     a.push_back(3);
     a.push_back(4);
     a.push_back(5);
+
+    //size
     cout<<a.size()<<endl;
     for(int i=0;i<a.size();i++){
         cout<<a[i]<<" ";
     }
+
+    //delete
+    a.pop_back();
+    
+    //capicity
+    cout<<a.capacity()<<endl;
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+
+    //first element 
+    cout<<a.front()<<endl;
+
+    //last element 
+    cout<<a.back()<<endl;
 }
