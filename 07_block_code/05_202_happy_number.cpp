eb61fab9d84c40055c9676f8b32f5925e4069fc4
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-18 14:38:29 +0530

    sorting

--- 07_block_code/05_202_happy_number.cpp
+++ 07_block_code/05_202_happy_number.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main(){
     int n,m=0,k,t=0,i=1;
     cin>>n;
-    while(i!=10){
-     while(n!=0){
-        m=n%10;
-        n=n/10;
+    int r=n;
+  
+     while(r!=0){
+        m=r%10;
+        r=r/10;
         k=m*m;
         t=t+k;
        
         cout<<t<<endl;
       
     }
-     n=t;
-     i++;
+     r=t;
+     
 }
 
-}
