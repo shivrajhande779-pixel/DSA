f05f548efc2501b6264e2929418cae361eeffefc
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-19 15:30:01 +0530

    leetcode

--- 09_leet_code/01_724_pivot_index.cpp
+++ 09_leet_code/01_724_pivot_index.cpp
@@ -4,4 +4,22 @@ using namespace std;
 int main(){
 
     int n;
+    cin>>n;
+    int num[n];
+
+    for(int i=0;i<n;i++){
+        cin>>num[i];
+    }
+    int total=0;
+    for(int i=0;i<5;i++){
+        total+=num[i];
+    }
+    int leftsum=0;
+    for( int i=0;i<5;i++){
+        int rightsum=total-leftsum-num[i];
+        if(leftsum==rightsum){
+            cout<<i;
+        }
+        leftsum+num[i];
+    }
 }
