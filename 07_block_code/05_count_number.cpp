043430ce3b50ddd0eb5dedddd92f55207b2e5693
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-23 13:42:42 +0530

    leetcode

--- 07_block_code/05_count_number.cpp
+++ 07_block_code/05_count_number.cpp
@@ -2,25 +2,34 @@
 using namespace std;
 
 int main(){
-    int n,k=0;
-    cin>>n;
-      int p=n;
-     int l=0;
-    while(n!=0){
-        int temp=n%10;
-        n=n/10;
-        k++;
+    // int n,k=0;
+    // cin>>n;
+    //   int p=n;
+    //  int l=0;
+    // while(n!=0){
+    //     int temp=n%10;
+    //     n=n/10;
+    //     k++;
       
        
-        while(p!=0){
-            int temp1=p%10;
-            p=p/10;
-            if(temp==temp1){
+    //     while(p!=0){
+    //         int temp1=p%10;
+    //         p=p/10;
+    //         if(temp==temp1){
                
-                l++;
-            }
-        }
+    //             l++;
+    //         }
+    //     }
          
+    // }
+    // cout<<"the found :"<<l;
+
+    int n;
+    cin>>n;
+    int a[n];
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
     }
-    cout<<"the found :"<<l;
+    
 }
