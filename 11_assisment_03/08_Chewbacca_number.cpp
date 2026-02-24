a1ae4212543a5bad82032e8bc82c3fc9605d4e1f
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-24 22:35:20 +0530

    leetcode

---
+++ 11_assisment_03/08_Chewbacca_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	int n,a=0,j=0,temp;
+	cin>>n;
+
+    while(n!=0){
+        temp=n%10;
+        n=n/10;
+        if(temp>4 && temp<9){
+            a=10-temp-1;
+         //   cout<<" "<<a;
+        }
+        //else cout<<" "<<temp;
+        j++;
+    `}
+	return 0;
+}
