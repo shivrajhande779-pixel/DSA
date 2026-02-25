a338c35de8ac5fa0c5b142bface689c564796e07
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-25 14:27:50 +0530

    leetcode

---
+++ 09_leet_code/07_42_trapping_rain.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[][4]={
+        {1,2,3,4},
+        {5,6,7,8},
+        {7,8,9,10}
+    };
+    int arr[2][3]={1,2,3,4,5,6};
+    int m,n;
+    cin>>m>>n;
+}
