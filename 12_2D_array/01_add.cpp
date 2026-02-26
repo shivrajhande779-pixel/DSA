731aa39e6f5f11089906854bbc84e56dbf60860d
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-02-26 15:08:42 +0530

    2d array

--- 12_2D_array/01_add.cpp
+++ 12_2D_array/01_add.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main(){
-    int n;
+    int m,n;
+    cin>>m>>n;
+    int a[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+     for(int i=0;i<m;i++){
+        if(i%2==0){
+        for(int j=0;j<n;j=j+2){
+            cout<<a[i][j]<<" ";
+        }
+    }
+                 else if(i%2!=0){
+                       for(int j=n;j>0;j=j-2){
+                            cout<<a[i][j]<<" ";
+                         }
+                        }
+     
+
+    }
 }
