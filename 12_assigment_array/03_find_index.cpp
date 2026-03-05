87c25af7a0f6b1216c7dbcbcbd8098fc074fe5e9
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-03-05 23:00:33 +0530

    assigment leet code

---
+++ 12_assigment_array/03_find_index.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(m==a[i]){
+            cout<<"number at "<<i<<" this inderx"
+        }
+    }
+}
