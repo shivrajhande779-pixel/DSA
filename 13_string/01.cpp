91285d15be90dadee1087dde10f035ae56409484
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-03-14 23:23:24 +0530

    tr

---
+++ 13_string/01.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int max=a[0];
+    for(int i=0;i<n;i++){
+        a[i]= a[i]*a[i];
+    }
+
+    for(int i=0;i<n;i++){
+        
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
