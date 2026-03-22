57c52ccb0550ab5abe854d2dacad214ec137de35
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-03-23 01:42:33 +0530

    test covert

--- 08_sorting/01_bubble_sort.cpp
+++ 08_sorting/01_bubble_sort.cpp
@@ -10,11 +10,9 @@ int main(){
     }
 
     for(int i=0;i<n;i++){
-        for(int j=0;j<n-i+1;j++){
+        for(int j=0;j<n-i-1;j++){
             if(a[j]>a[j+1]){
-                int temp= a[i];
-                a[i]=a[j];
-                a[j]=temp;
+               swap(a[j],a[j+1]);
             }
         }
     }
--- 12_2D_array/04_anti_clock_print.cpp
+++ 12_2D_array/04_anti_clock_print.cpp
@@ -13,36 +13,31 @@ int main (){
 	}
     int i=0;
     int top=0,bottom=m,left=n,right=0;
-    while(top==bottom){
+    while (top <= bottom and left >= right){
 
         //right
         for(int j=top;j<bottom;j++){
             cout<<a[j][i]<<" ";
         }
         right++;
-        cout<<" : right"<<endl;
 
         //bottom
         for(int b=right;b<left;b++){   
             cout<<a[bottom][b]<<" ";
         }
         bottom--;
-        cout<<" : bottom"<<endl;
-
 
         // left
         for(int l=bottom;l>=0;l--){
             cout<<a[l][n]<<" ";
         }
         left--;
-        cout<<" : left"<<endl;
         
         //top
         for(int t=left;t>=0;t--){
             cout<<a[i][left]<<" ";
         }
         top++;
-         cout<<" : top"<<endl;
         i++;
     }
 }
