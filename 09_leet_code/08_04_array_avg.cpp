b8232ddeabaeec5274ea15ff92ac3ea1e7283020
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-03-28 15:34:44 +0530

    leetcode

---
+++ 09_leet_code/08_04_array_avg.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int m,n,p;
+    cin>>m>>n;
+    int a[m];
+    int b[n];
+    
+    for(int i=0;i<m;i++){
+        cin>>a[i];
+    }
+      for(int i=0;i<n;i++){
+        cin>>s[i];
+    }
+     int q=a.size();
+        int w=b.size();
+        int c=q+w;
+
+        cout<<c;
+
+}
