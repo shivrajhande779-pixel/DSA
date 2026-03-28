60d9b5c37dcdd7258c8c5fca9651bba625e6e890
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-03-28 18:34:18 +0530

    practical

--- 09_leet_code/08_04_array_avg.cpp
+++ 09_leet_code/08_04_array_avg.cpp
@@ -1,22 +1,53 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    int m,n,p;
-    cin>>m>>n;
-    int a[m];
-    int b[n];
-    
-    for(int i=0;i<m;i++){
-        cin>>a[i];
-    }
-      for(int i=0;i<n;i++){
-        cin>>s[i];
-    }
-     int q=a.size();
-        int w=b.size();
-        int c=q+w;
-
-        cout<<c;
-
+    string s;
+    cin>>s;
+        long long n=0;
+        for(int i=0;i<s.length();i++){
+            char p=s[];
+            char ch=s[i];
+            if(ch=='I' ){
+                n=n+1;
+            }
+            else if(ch=='V'){
+                if(p=='I'){
+                    n=n+5-2;
+                }
+                else n=n+5;
+            } 
+            else if(ch=='X'){
+                if(p=='I'){
+                    n=n+10-2;
+                }
+                else n=n+10;
+            }
+            else if(ch=='L'){
+                if(p=='X'){
+                    n=n+50-20;
+                }
+                 else n=n+50;
+            }
+            else if(ch=='C'){
+                if(p=='X'){
+                     n=n+100-20;
+                }
+                else n=n+100;
+            }
+            else if(ch=='D'){
+                if(p=='C'){
+                     n=n+500-200;
+                }
+                else
+                n=n+500;
+            }
+            else if(ch=='M'){
+                if(p=='C'){
+                     n=n+1000-200;
+                }
+                else
+                n=n+1000;
+            }
+        }
+        cout<<n;
 }
