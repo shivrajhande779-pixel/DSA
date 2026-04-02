9326ec9a82ff9e089772ff9898917514f2285400
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-04-02 23:44:42 +0530

    tt

--- 06_vector/03_find_last_number.cpp
+++ 06_vector/03_find_last_number.cpp
@@ -9,4 +9,9 @@
     for(int i =0;i<a.size();i++){
         v.push_back(a[i]);
     }
+     vector<int> p;
+    int f[5]={1,2,3,4,5}
+    for(int i =0;i<a.size();i++){
+        p.push_back(f[i]);
+    }
 }
