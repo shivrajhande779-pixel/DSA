49538b86e9fd5daf2660043b0fb2425332e9ff9f
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-04-03 23:44:46 +0530

    leet code

--- 06_vector/03_find_last_number.cpp
+++ 06_vector/03_find_last_number.cpp
@@ -5,18 +5,18 @@
  int main(){
 
     vector<int> v;
-    int a[5]={1,2,3,4,5}
-    for(int i =0;i<a.size();i++){
+    int a[5]={1,2,3,4,5};
+    for(int i =0;i<v.size();i++){
         v.push_back(a[i]);
     }
      vector<int> p;
-    int f[5]={1,2,3,4,5}
-    for(int i =0;i<a.size();i++){
-        p.push_back(f[i]);
+    int f[5]={1,2,3,4,5};
+    for(int w =0;w<p.size();w++){
+        p.push_back(f[w]);
     }
      vector<int> r;
-    int q[5]={1,2,3,4,5}
-    for(int i =0;i<a.size();i++){
+    int q[5]={1,2,3,4,5};
+    for(int i =0;i<r.size();i++){
         r.push_back(q[i]);
     }
 }
