146f623cfc05d74e1312f618fc5bcafa6b3ad4c2
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-04-08 23:54:23 +0530

    pra

---
+++ 10_lecture/03_remove_duplicate.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {1, 2, 2, 3, 4, 4, 5};
+
+    sort(v.begin(), v.end()); // Step 1: sort
+    v.erase(unique(v.begin(), v.end()), v.end()); // Step 2: remove duplicates
+
+    for (int x : v) {
+        cout << x << " ";
+    }
+}
