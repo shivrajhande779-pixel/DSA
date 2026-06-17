18a2008e705c4cb179c4a87655814973acedc24a
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-06-17 12:16:16 +0530

    sd

--- operation.js
+++ operation.js
@@ -7,8 +7,12 @@ const lower=name.map(word => word.toUpperCase(name));
 console.log(lower);
 
 
+// print only you want
 let product=[
     { id: 1,name : "laptop"},
-     { id: 1,name : "laptop"},
-      { id: 1,name : "laptop"},
+     { id: 1,name : "samrtphone"},
+      { id: 1,name : "TV"},
 ]
+const q2=product.map(product =>product.name);
+console.log(q2);
+
