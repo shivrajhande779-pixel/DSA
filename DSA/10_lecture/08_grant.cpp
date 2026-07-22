4393ad6d5a07a03110f3b21ada3bfbc128f71b95
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-07-22 22:23:21 +0530

    practies

---
+++ DSA/10_lecture/08_grant.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int n,k=0;
+    // cin>>n;
+    //   int p=n;
+    //  int l=0;
+    // while(n!=0){
+    //     int temp=n%10;
+    //     n=n/10;
+    //     k++;
+      
+       
+    //     while(p!=0){
+    //         int temp1=p%10;
+    //         p=p/10;
+    //         if(temp==temp1){
+               
+    //             l++;
+    //         }
+    //     }
+         
+    // }
+    // cout<<"the found :"<<l;
+
+    int n;
+    cin>>n;
+    int a[n];
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+}
