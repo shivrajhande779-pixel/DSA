d83bd3c6b6211a911d5677d44c418df3d150a786
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-07-28 13:53:32 +0530

    practies

---
+++ DSA/05_array/09_show.cpp
@@ -0,0 +1,17 @@
+#ikclude<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter the array element : ";
+        cin>>a[i];
+    }
+    cout<<endl;
+      for(int i=0;i<n;i++){
+        cout<<"array element is : "<<a[i];
+        cout<<endl;
+    }
+}
