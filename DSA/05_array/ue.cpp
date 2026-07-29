9c5a2a8fe1622a769bf72afa02267c00b1a425b7
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-07-30 00:00:14 +0530

    fcd

---
+++ DSA/05_array/ue.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr)
+            return true;
+
+        if (left == nullptr || right == nullptr)
+            return false;
+
+        if (left->val != right->val)
+            return false;
+
+        return isMirror(left->left, right->right) &&
+               isMirror(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+
+        return isMirror(root->left, root->right);
+    }
+};
+
+int main() {
+    // Example 1:
+    //        1
+    //      /   \
+    //     2     2
+    //    / \   / \
+    //   3  4  4   3
+
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(3);
+
+    Solution obj;
+
+    if (obj.isSymmetric(root))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+
+    return 0;
+}
