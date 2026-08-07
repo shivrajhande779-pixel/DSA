d624e528fcf07fad5052472a77937447c72ac059
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-07 23:49:06 +0530

    leet

--- DSA/09_leet_code/88_leetcode.cpp
+++ DSA/09_leet_code/88_leetcode.cpp
@@ -41,7 +41,7 @@ int main() {
 
     // Test Case 1 - PASS
     vector<int> nums1 = {1, 2, 3, 0, 0, 0};
-    vector<int> nums2 = {2, 5, 6};
+    vector<int> nums2 = {2, 5, 1};
 
     s.merge(nums1, 3, nums2, 3);
 
