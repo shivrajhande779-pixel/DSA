a556d946f3bca7fdcaea74d2788dddd4929d5e5e
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-08 23:54:27 +0530

    leetcode

---
+++ DSA/09_leet_code/41_leetcode.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 &&
+                   nums[i] <= n &&
+                   nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+};
+
+int main() {
+    Solution s;
+
+    vector<int> nums = {3, 4, -1, 1};
+
+    int ans = s.firstMissingPositive(nums);
+
+    cout << "First Missing Positive = " << ans << endl;
+
+    return 0;
+}
