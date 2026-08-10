de96765d1bbe8a2bdfee70f8a1013a49f34a47cd
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-10 19:28:20 +0530

    leetode

---
+++ DSA/09_leet_code/20_leetcode_new.cpp
@@ -0,0 +1,37 @@
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+
+            // Opening brackets
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+            // Closing brackets
+            else {
+                if (st.empty())
+                    return false;
+
+                char top = st.top();
+                st.pop();
+
+                if (c == ')' && top != '(')
+                    return false;
+
+                if (c == '}' && top != '{')
+                    return false;
+
+                if (c == ']' && top != '[')
+                    return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
