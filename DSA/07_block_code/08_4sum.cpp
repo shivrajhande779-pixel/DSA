e320d1e0962db792a6e5379983dddc57141edd82
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-15 00:58:36 +0530

    leetcode

---
+++ DSA/07_block_code/08_4sum.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 1; j < n - 2; j++) {
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    long long sum = (long long)nums[i]
+                                  + nums[j]
+                                  + nums[left]
+                                  + nums[right];
+
+                    if (sum == target) {
+                        ans.push_back({
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        });
+
+                        left++;
+                        right--;
+                    }
+                    else if (sum < target) {
+                        left++;
+                    }
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    Solution s;
+
+    vector<int> nums = {1, 0, -1, 0, -2, 2};
+    int target = 0;
+
+    vector<vector<int>> result = s.fourSum(nums, target);
+
+    cout << "Output:" << endl;
+
+    for (auto quad : result) {
+        cout << "{ ";
+        for (int x : quad) {
+            cout << x << " ";
+        }
+        cout << "}" << endl;
+    }
+
+    return 0;
+}
