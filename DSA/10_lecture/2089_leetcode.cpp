880710016f31c3027fd9c5cedb9cdb76394f6748
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-17 23:00:24 +0530

    practies

---
+++ DSA/10_lecture/2089_leetcode.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {1, 2, 5, 2, 3};
+    int target = 2;
+
+    vector<int> result = obj.targetIndices(nums, target);
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
