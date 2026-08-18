bf2bf581fd03edaaeaebef03ce3573b6b13706ce
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-18 14:17:23 +0530

    practies

---
+++ DSA/10_lecture/12_array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int findMax(int arr[], int n) {
+    int mx = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > mx) {
+            mx = arr[i];
+        }
+    }
+
+    return mx;
+}
+
+int main() {
+    srand(time(0));
+
+    int n = 10;
+    int arr[10];
+
+    cout << "Random Array: ";
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100 + 1;
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nMaximum value = " << findMax(arr, n);
+
+    return 0;
+}
