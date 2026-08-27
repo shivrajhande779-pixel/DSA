f7f3d83a2ba4ef2dcca67db51c46bf839947b273
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-27 23:40:01 +0530

    non rep char

---
+++ DSA/03_practies/09_non_repeating_char.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    string s;
+
+    cout << "Enter a string: ";
+    cin >> s;
+
+    unordered_map<char, int> freq;
+
+    // Count frequency
+    for (char ch : s) {
+        freq[ch]++;
+    }
+
+    // Find first character with frequency 1
+    for (char ch : s) {
+        if (freq[ch] == 1) {
+            cout << "First non-repeating character = " << ch << endl;
+            return 0;
+        }
+    }
+
+    cout << "No non-repeating character found." << endl;
+
+    return 0;
+}
