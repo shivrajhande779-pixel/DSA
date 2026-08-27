729298f325001d46eeefefe46d389ceaec1f3919
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-27 23:26:44 +0530

    p

--- DSA/08_sorting/01_bubble.cpp
+++ DSA/08_sorting/01_bubble.cpp
@@ -9,7 +9,7 @@ int main(){
         cin>>a[i];
     }
 
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<n;i++){
         for(int j=0;j<n-i+1;j++){
             if(a[j]>a[j+1]){
                 int temp= a[i];
