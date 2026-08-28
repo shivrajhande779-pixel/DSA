f4064e6f4d2477f2a3ea046248119b3e493c1c8f
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-28 23:53:12 +0530

    practies

---
+++ DSA/03_practies/10_spiral_matrix.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter rows: ";
+    cin >> rows;
+
+    cout << "Enter columns: ";
+    cin >> cols;
+
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+
+    cout << "Enter matrix elements:\n";
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int top = 0;
+    int bottom = rows - 1;
+    int left = 0;
+    int right = cols - 1;
+
+    cout << "\nSpiral Order: ";
+
+    while (top <= bottom && left <= right) {
+
+        // Left -> Right
+        for (int j = left; j <= right; j++)
+            cout << matrix[top][j] << " ";
+
+        top++;
+
+        // Top -> Bottom
+        for (int i = top; i <= bottom; i++)
+            cout << matrix[i][right] << " ";
+
+        right--;
+
+        // Right -> Left
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--)
+                cout << matrix[bottom][j] << " ";
+
+            bottom--;
+        }
+
+        // Bottom -> Top
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                cout << matrix[i][left] << " ";
+
+            left++;
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
---
+++ full_stack/project 1/04_js/01_object_latral.js
@@ -0,0 +1,36 @@
+const post={
+    username: "@lunerfaade",
+    followers: 365,
+    following: 360,
+    content: "this is my #first post",
+    like: 200
+}
+
+// add or update
+
+const student={
+    name: "shivraj",
+    class: 16,
+    city:"indore",
+    age: 21
+}
+
+// array of obejects
+
+const classinfo= [
+    {
+        name: "shivraj",
+        grade: "A+",
+        city: "washim"
+    },
+    {
+        name: "khushi",
+        grade: "O",
+        city: "amravti"
+    },
+    {
+        name: "mamta",
+        grade: "A",
+        city: "jalna"
+    },
+]
