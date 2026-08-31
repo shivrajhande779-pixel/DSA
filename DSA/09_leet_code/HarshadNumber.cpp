618a8854838f5665843a82c36630873162894fa1
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-08-31 22:01:27 +0530

    Harshad Number

---
+++ DSA/09_leet_code/HarshadNumber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int num = x;
+        while(x>0){
+            int digit = x%10;
+            sum = sum + digit;
+            x = x/10;
+        }
+        if(num%sum==0){
+            return sum;
+        }
+        else return -1;
+    }
+}
