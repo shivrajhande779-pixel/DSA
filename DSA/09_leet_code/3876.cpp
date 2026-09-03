9c1750b77b66cddc10dceab09e03ff431eb98222
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-09-03 23:07:07 +0530

    h

--- DSA/09_leet_code/3876.cpp
+++ DSA/09_leet_code/3876.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int mn = *min_element(nums1.begin(), nums1.end());
-
+        bool hasOdd = false;
+        bool hasEven = false;
         for (int x : nums1) {
-            if ((x - mn) % 2 != 0) {
-                return false;
-            }
+            if (x % 2 == 0)
+                hasEven = true;
+            else
+                hasOdd = true;
         }
-
-        return true;
+        if (!hasOdd || !hasEven)
+            return true;
+        int mn = *min_element(nums1.begin(), nums1.end());
+        if (mn % 2 == 1)
+            return true;
+        return false;
     }
 };
