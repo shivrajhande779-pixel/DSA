849416aa423fb15491531bd1dec33ac3855ae9f4
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-09-13 23:39:19 +0530

    practies

---
+++ DSA/03_practies/15_2nd_largest.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    int second = -1;
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    if(second == -1) {
+        cout << "Second largest element does not exist";
+    }
+    else {
+        cout << "Second largest element = " << second;
+    }
+
+    return 0;
+}
