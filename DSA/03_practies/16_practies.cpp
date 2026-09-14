4fb649cdc84aeb38f9557b2d07d24ccc57616539
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-09-14 23:51:28 +0530

    practies

---
+++ DSA/03_practies/16_practies.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int count = 1;
+    int maxCount = 1;
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > arr[i - 1]) {
+            count++;
+        }
+        else {
+            count = 1;
+        }
+
+        if(count > maxCount) {
+            maxCount = count;
+        }
+    }
+
+    cout << "Longest increasing sequence length = " << maxCount;
+
+    return 0;
+}
