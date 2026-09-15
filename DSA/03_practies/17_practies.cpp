baca684c3cb973c8506557cec608d7da37d84ba4
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-09-16 02:01:20 +0530

    practies

---
+++ DSA/03_practies/17_practies.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int mergeStones(vector<int>& stones, int k) {
+    int n = stones.size();
+
+    if ((n - 1) % (k - 1) != 0)
+        return -1;
+
+    vector<int> prefix(n + 1, 0);
+
+    for (int i = 0; i < n; i++) {
+        prefix[i + 1] = prefix[i] + stones[i];
+    }
+
+    const int INF = 1000000000;
+
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    for (int len = k; len <= n; len++) {
+        for (int i = 0; i + len <= n; i++) {
+            int j = i + len - 1;
+
+            dp[i][j] = INF;
+
+            for (int mid = i; mid < j; mid += k - 1) {
+                dp[i][j] = min(dp[i][j],
+                               dp[i][mid] + dp[mid + 1][j]);
+            }
+
+            if ((len - 1) % (k - 1) == 0) {
+                dp[i][j] += prefix[j + 1] - prefix[i];
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+}
+
+int main() {
+    int n, k;
+
+    cout << "Enter number of piles: ";
+    cin >> n;
+
+    vector<int> stones(n);
+
+    cout << "Enter stone piles: ";
+    for (int i = 0; i < n; i++) {
+        cin >> stones[i];
+    }
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    int answer = mergeStones(stones, k);
+
+    cout << "Minimum Cost = " << answer << endl;
+
+    return 0;
+}
