b21a5bb097bddbd8f3053990f73d7f8875821bd9
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-09-20 23:55:24 +0530

    practies

--- DSA/03_practies/19_add_array.cpp
+++ DSA/03_practies/19_add_array.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << "Enter size of array: ";
     cin >> n;
 
-    int arr[100];
+    int arr[99999];
 
     cout << "Enter " << n << " elements: ";
     for(int i = 0; i < n; i++) {
