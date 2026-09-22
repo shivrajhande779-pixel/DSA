faea7bce00e0aad626c90e8b62bb701230dc7d4c
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-09-22 23:49:57 +0530

    practies

---
+++ DSA/09_leet_code/229_majority_ele.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    vector<int> arr(n);
+    
+    cout<<"Enter the elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int m=arr.size();
+    cout<<m;
+     
+}
