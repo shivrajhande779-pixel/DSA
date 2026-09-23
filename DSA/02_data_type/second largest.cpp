302abde544d74fa6ef8f5ad815115a7474584372
shivrajhande779-pixel <shivrajhande779@gmail.com>
2026-09-23 23:57:33 +0530

    pract

--- DSA/02_data_type/second largest.cpp
+++ DSA/02_data_type/second largest.cpp
@@ -18,7 +18,7 @@ int main() {
     int largest = arr[0];
     int secondLargest = -1;
 
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n+1; i++) {
         if (arr[i] > largest) {
             secondLargest = largest;
             largest = arr[i];
---
+++ full_stack/project 1/04_js/03_random color.js
@@ -0,0 +1,26 @@
+let btn= document.querySelector("button");
+
+
+btn.addEventListener("click", function() {
+
+    let h3= document.querySelector("h3");
+    let randomColor= getRandomColor();
+    h3.innerText = randomColor;
+
+    let div= document.querySelector("div");
+    div.style.backgroundColor= randomColor;
+
+    btn.style.backgroundColor= randomColor;
+    btn.style.color= "white";
+
+    console.log("color changed");
+});
+
+function getRandomColor() {
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+
+    let color= `rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
